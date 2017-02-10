2d68b7fa83595ef1fd3bf5c0c99bea2a425d4244
Erica Millado <ericamillado@gmail.com>
2017-02-10 17:27:16 -0500

    Add tab bar controller

--- CCR/ViewController.swift
+++ CCR/ViewController.swift
@@ -9,16 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = Timer()
+    var counter = 0
+    var timerIsOn = false
+    
+    @IBOutlet weak var countingLabel: UILabel!
+    
+    
+    @IBAction func playBtnPressed(_ sender: Any) {
+        
+//        if !timerIsOn {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateCounter), userInfo: nil, repeats: true)
+//            timerIsOn = true
+//        }
+        
+    }
 
+    @IBAction func pauseBtnPressed(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func clearBtnPressed(_ sender: Any) {
+        timer.invalidate()
+        counter = 0
+        countingLabel.text = "\(counter)"
+//        timerIsOn = false
+    }
+    
+    func updateCounter() {
+        counter += 1
+        countingLabel.text = "\(counter)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
