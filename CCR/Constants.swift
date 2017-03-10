cde03373406c2762a7c86819ae591a72cefe9377
Erica Millado <ericamillado@gmail.com>
2017-03-09 23:22:34 -0500

    Saving

--- CCR/Constants.swift
+++ CCR/Constants.swift
@@ -15,6 +15,8 @@ struct Constants {
     static let red = UIColor(red:1.00, green:0.32, blue:0.32, alpha:1.0)
     //##EFF1F3 
     static let grey = UIColor(red:0.94, green:0.95, blue:0.95, alpha:1.0)
+    //#DDDBDD
+    static let mediumGrey = UIColor(red:0.87, green:0.86, blue:0.87, alpha:1.0)
     //#00856A
     static let aqua = UIColor(red:0.00, green:0.52, blue:0.42, alpha:1.0)
    
--- CCR/OnboardingViewController.swift
+++ CCR/OnboardingViewController.swift
@@ -25,23 +25,32 @@ class OnboardingViewController: UIViewController, PaperOnboardingDataSource {
     
     //tells how many onboarding screens you want; can also indicate this in .plist
     func onboardingItemsCount() -> Int {
-//        return 3
-        return 1
+        return 3
+//        return 1
     }
     
     //enables us to configure each onboarding item
     func onboardingItemAtIndex(_ index: Int) -> OnboardingItemInfo {
-        let backgroundColor1 = UIColor.white
+        let backgroundColor1 = UIColor.black
         let backgroundColor2 = Constants.aqua
         let backgroundColor3 = Constants.red
         let titleFont = UIFont(name: "OpenSans-Bold", size: 30)!
-        let descriptionFont = UIFont(name: "OpenSans-SemiBold", size: 18)!
+        let descriptionFont = UIFont(name: "OpenSans-SemiBold", size: 17)!
         
         // (imageName: String, title: String, description: String, iconName: String, color: UIColor, titleColor: UIColor, descriptionColor: UIColor, titleFont: UIFont, descriptionFont: UIFont)
         
-        return [("Goal Filled-100red", "Set a Goal", "Think of major project you want to work on.  It can be a new app idea or a side project.\nExample: MAKE A PERSONAL WEBSITE.", "", backgroundColor1, UIColor.black, UIColor.black, titleFont, descriptionFont),
+        return [("Goal Filled-100", "Set a Goal", "Think of major project you want to work on.  It can be a new app idea or a side project.\nExample: MAKE A PERSONAL WEBSITE.", "", backgroundColor1, Constants.red, Constants.red, titleFont, descriptionFont),
                 ("Timer Filled-100", "Do a 25 Min. Sprint", "For your goal, decide on ONE task you need to work on to accomplish your goal.\nExample: CREATE WIREFRAMES FOR WEBSITE.", "", backgroundColor2, UIColor.white, UIColor.white, titleFont, descriptionFont),
                 ("Todo List-100", "Reflect on your Progress", "You'll rate each task interval you complete and be able see your progress over time.", "", backgroundColor3, UIColor.white, UIColor.white, titleFont, descriptionFont)][index]
-        }
+    }
+    
+//    Tells the delegate the PaperOnboarding is about to draw a item for a particular row. Use this method for configure items
+//    
+//    - parameter item:  A OnboardingContentViewItem object that PaperOnboarding is going to use when drawing the row.
+//    - parameter index: An curretn index item
+//    */
+    func onboardingConfigurationItem(_ item: OnboardingContentViewItem, index: Int) {
+        
+    }
 
 }
