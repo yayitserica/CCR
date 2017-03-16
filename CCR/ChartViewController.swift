809838ea13e0d0520f33c67b2493918abb67ea95
Erica Millado <ericamillado@gmail.com>
2017-03-16 09:52:39 -0400

    Got charts working initially

--- CCR/ChartViewController.swift
+++ CCR/ChartViewController.swift
@@ -7,29 +7,43 @@
 //
 
 import UIKit
+import Charts
 
 class ChartViewController: UIViewController {
+    
+    let store = DataStore.sharedInstance
 
+    @IBOutlet weak var barChartView: BarChartView!
+    
+    var months: [String]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        setChart(dataPoints: months, values: unitsSold)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        barChartView.noDataText = "There is no data available for the chart."
+        //for the chart to display data, we need to create a barchartdataobject
+        //1 - create an array of BarChartDataEntry objects
+        var dataEntries: [BarChartDataEntry] = []
+        for i in 0..<dataPoints.count {
+            //2 - initialize an instance of BarChartDataEntry that takes in the value of each entry (y value) and the index (x value)
+            let dataEntry = BarChartDataEntry(x: Double(i), y: values[i])
+            dataEntries.append(dataEntry)
+        }
+        //3 - we use our data entries and create a BarChartData object
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Units Sold")
+        //4 - we set this data set as our chart view's data
+        let chartData = BarChartData(dataSet: chartDataSet)
+        barChartView.data = chartData
+        //5 - change the default color of the bar chart
+        chartDataSet.colors = [Constants.red, Constants.grey, Constants.mediumGrey]
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
